65d5f7a5400d2e8f6d749d0b71ba4477b4a3a1b5
UBER TORO <alonso.toro@est.iudigital.edu.co>
2026-01-22 01:05:02 -0500

    actividad_1 - configuracion

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pad",
+    version="0.0.1",
+    author="ubert toro",
+    author_email="alonso.toro@est.iudigital.edu.co",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "matplotlib"
+    ]
+    
+    
+)
